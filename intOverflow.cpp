84ef1522744fadd6050c833556db7650952be569
Maxim Bondarenko <mgbondarenko@luxoft.com>
2016-05-23 16:31:45 +0300

    integer tipe prefix, sizeof 42L

--- intOverflow.cpp
+++ intOverflow.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char** argv)
 {
+   std::cout << "Sizeof 34: " << sizeof(34) << std::endl;
+   std::cout << "Sizeof 34LL: " << sizeof(34LL) << std::endl;
+   std::cout << "Sizeof uintmax_t: " << sizeof(uintmax_t) << std::endl;
+
    int32_t timestamp = 0x3fffffff;
    std::cout << timestamp << std::endl;
 
