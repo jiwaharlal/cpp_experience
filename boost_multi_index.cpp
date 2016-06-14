e77c9fe5e5c2ddd0cddc84ad91a2afa742808ee8
Maxim Bondarenko <maxim.bondarenko@gmail.com>
2016-06-14 07:09:32 +0300

    boost multi_index

--- boost_multi_index.cpp
+++ boost_multi_index.cpp
@@ -2,7 +2,10 @@
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <set>
 
 using boost::multi_index_container;
 using namespace boost::multi_index;
@@ -16,6 +19,19 @@ struct RequestInfo
    int requestId;
    bool isForRoute;
    bool isIndependent;
+};
+
+struct TileIdComparator
+{
+   bool operator ()(const RequestInfo& ri, int tileId)
+   {
+      return ri.tileId < tileId;
+   }
+
+   bool operator ()(int tileId, const RequestInfo& ri)
+   {
+      return tileId < ri.tileId;
+   }
 };
 
 std::ostream& operator<<(std::ostream& out, const RequestInfo& i)
@@ -43,11 +59,16 @@ int main(int argc, char** argv)
    tRequestMap requests;
 
    RequestInfo r1 = {1, 4, true, false};
+   RequestInfo r2 = {3, 3, true, true};
+   RequestInfo r3 = {2, 5, false, false};
 
    requests.insert(r1);
+   requests.insert(r2);
+   requests.insert(r3);
 
    //tRequestMap::index_const_iterator<tileId> = get<tileId>(
    const typename boost::multi_index::index<tRequestMap, tileId>::type& tileIdMap = get<tileId>(requests);
+   const typename boost::multi_index::index<tRequestMap, requestId>::type& reqIdMap = get<requestId>(requests);
 
    bool isForRoute = tileIdMap.find(1)->isForRoute;
    std::cout << isForRoute << std::endl;
@@ -56,8 +77,31 @@ int main(int argc, char** argv)
    std::cout << *(tileIdMap.find(3)) << std::endl;
 
    std::cout << requests.size() << std::endl;
-   requests.erase(tileIdMap.find(1));
-   std::cout << requests.size() << std::endl;
+   //requests.erase(tileIdMap.find(1));
+   //std::cout << requests.size() << std::endl;
+
+   std::cout << *(tileIdMap.begin()) << std::endl;
+   std::cout << *(requests.begin()) << std::endl;
+
+   // on getMapData
+   tiles = tileCalculator.calculate();
+
+
+
+
+
+   std::cout << "===========================" << std::endl;
+   std::copy(tileIdMap.begin(), tileIdMap.end(), std::ostream_iterator<const RequestInfo&>(std::cout, "\n"));
+   std::cout << "===========================" << std::endl;
+   std::copy(reqIdMap.begin(), reqIdMap.end(), std::ostream_iterator<const RequestInfo&>(std::cout, "\n"));
+
+   std::set<int> tilesToDrop = {2, 3};
+   tRequestMap rm2;
+   std::vector<RequestInfo> rv;
+   std::set_difference(tileIdMap.begin(), tileIdMap.end(), tilesToDrop.begin(), tilesToDrop.end(), std::inserter(rm2, rm2.end()), TileIdComparator());
+
+   std::cout << "===========================" << std::endl;
+   std::copy(rm2.begin(), rm2.end(), std::ostream_iterator<const RequestInfo&>(std::cout, "\n"));
 
    return 0;
 }
