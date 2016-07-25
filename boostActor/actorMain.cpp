f9a8fe8d444db59ae49d19a6adc486264dd44736
Maxim Bondarenko <bondarenko.maxim@gmail.com>
2016-07-25 09:09:29 +0300

    boost::mpl - based visitor

--- boostActor/actorMain.cpp
+++ boostActor/actorMain.cpp
@@ -1,9 +1,22 @@
 //#include <boost/asio.hpp>
 //#include <boost/signals2.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include <boost/range/algorithm.hpp>
+#include <boost/foreach.hpp>
+#include <boost/thread.hpp>
 #include <boost/thread/condition_variable.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/mpl/apply.hpp>
 #include <boost/mpl/list.hpp>
 #include <boost/mpl/vector.hpp>
+#include <boost/mpl/greater_equal.hpp>
+#include <boost/mpl/and.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/pop_front.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/size.hpp>
 #include <boost/type_index.hpp>
 #include <boost/variant.hpp>
 #include <boost/variant/detail/over_sequence.hpp>
@@ -15,22 +28,58 @@
 template <typename TypeList>
 class CActor;
 
-template <typename T>
+template <typename T, typename = void>
 class IHandler
 {
 public:
-   void operator ()(const T& msg) { on(msg); }
-   virtual void on(const T&) = 0;
+   void operator ()(T msg) { on(msg); }
+   virtual void on(T) = 0;
+   virtual void post(T) = 0;
 };
 
-struct FirstMessage{};
-struct SecondMessage{};
-struct ThirdMessage{};
+template <typename T>
+class IHandler
+<
+   T,
+   typename boost::enable_if
+   <
+      boost::mpl::and_
+      <
+         boost::mpl::is_sequence<T>,
+         boost::mpl::greater_equal
+         <
+            boost::mpl::size<T>,
+            boost::mpl::int_<2>
+         >
+      >
+   >::type
+>  : public IHandler<typename boost::mpl::front<T>::type>
+   , public IHandler<typename boost::mpl::pop_front<T>::type>
+{};
+
+template <typename T>
+class IHandler
+<
+   T,
+   typename boost::enable_if
+   <
+      boost::mpl::and_
+      <
+         boost::mpl::is_sequence<T>,
+         boost::mpl::equal_to
+         <
+            boost::mpl::size<T>,
+            boost::mpl::int_<1>
+         >
+      >
+   >::type
+>  : public IHandler<typename boost::mpl::front<T>::type>
+{};
 
 struct HandlerListBase {};
 
 template <typename MessageType>
-struct HandlerList
+struct HandlerList: public HandlerListBase
 {
    std::set<IHandler<MessageType> > mHandlers;
 };
@@ -41,40 +90,86 @@ public:
    template <typename MessageType>
    void subscribe(IHandler<MessageType>& handler)
    {
-      std::string typeName = boost::typeindex::type_id<FirstMessage>().raw_name();
+      std::string typeName = boost::typeindex::type_id<MessageType>().raw_name();
       boost::shared_ptr<HandlerListBase> hl = mHandlerMap[typeName];
       if (!hl)
       {
          hl.reset(new HandlerList<MessageType>());
       }
-      std::set<IHandler<MessageType> >& handlers = static_cast<HandlerList<MessageType> >(hl)->mHandlers;
+      std::set<IHandler<MessageType> >& handlers = static_cast<HandlerList<MessageType>* >(hl.get())->mHandlers;
       handlers.insert(handler);
    }
 
    //template <typename MessageType>
    //void unsubscribe(IHandler<MessageType>& handler);
 
-   //template <typename MessageType>
-   //void publish(boost::shared_ptr<MessageType> message);
+   template <typename MessageType>
+   void publish(MessageType message)
+   {
+      typedef std::set<IHandler<MessageType> > tHandlerSet;
+      std::string typeName = boost::typeindex::type_id<MessageType>().raw_name();
+      boost::shared_ptr<HandlerListBase> hl = mHandlerMap[typeName];
+      if (!hl)
+      {
+         hl.reset(new HandlerList<MessageType>());
+      }
+      std::set<IHandler<MessageType> >& handlers = static_cast<HandlerList<MessageType>* >(hl.get())->mHandlers;
+      BOOST_FOREACH(typename tHandlerSet::reference handler, handlers)
+      {
+         handler.post(message);
+      }
+      //boost::range::for_each(
+            //static_cast<HandlerList<MessageType>* >(hl.get())->mHandlers,
+            //boost::lambda::bind(&IHandler<MessageType>::post(message), _1) );
+      //BOOST_FOREACH(IHandler<MessageType>& handler, static_cast<HandlerList<MessageType>* >(hl.get())->mHandlers)
+      //{
+         //handler.post(message);
+      //}
+   }
 private:
-   std::map<int, std::string> stringMap;
-   std::map<std::string, boost::shared_ptr<HandlerListBase> > mHandlerMap;
+   typedef std::map<std::string, boost::shared_ptr<HandlerListBase> > tHandlerMap;
+   tHandlerMap mHandlerMap;
+};
+
+template <typename T>
+struct add_shared_ptr
+{
+   typedef boost::shared_ptr<T> type;
+};
+
+template<class Seq>
+struct shared_ptr_variant
+{
+    typedef typename boost::make_variant_over<
+            typename boost::mpl::transform<
+                Seq, add_shared_ptr<boost::mpl::_1>
+            >::type
+        >::type type;
 };
 
 template <typename TypeList>
-class CActor
+class CActor: public IHandler<typename boost::mpl::transform<TypeList, add_shared_ptr<boost::mpl::_1>::type>::type>
 {
 public:
-   CActor(CBoard& board);
+   CActor(CBoard& board) : mBoard(board) {}
 
-   void run();
-   void stop();
+   void run()
+   {
+      mIsStop = false;
+      mThread = boost::thread(boost::bind(&CActor::threadFunction, this));
+   }
+
+   void stop()
+   {
+      mIsStop = true;
+      mCondition.notify_one();
+   }
 
    template <typename MessageType>
    void post(boost::shared_ptr<MessageType> message)
    {
       mMessages.push(tMsgVariant(message));
-      cw.notify_all();
+      mCondition.notify_one();
    }
 
 private:
@@ -83,9 +178,13 @@ private:
       while (true)
       {
          boost::mutex m;
-         boost::lock_guard<boost::mutex> lk(m);
+         boost::unique_lock<boost::mutex> lk(m);
 
-         cw.wait(m);
+         mCondition.wait(lk);
+         if (mIsStop)
+         {
+            break;
+         }
 
          tMsgVariant msg = mMessages.pop();
          boost::apply_visitor(*this, msg);
@@ -93,25 +192,49 @@ private:
    }
 
 private:
-   typedef boost::variant<boost::detail::variant::over_sequence<TypeList> > tMsgVariant;
+   typedef typename shared_ptr_variant<TypeList>::type tMsgVariant;
    typedef std::queue<tMsgVariant> tMsgQueue;
 
 private:
    tMsgQueue mMessages;
-   boost::condition_variable cw;
+   boost::condition_variable mCondition;
+   CBoard& mBoard;
+   boost::thread mThread;
+   boost::atomic<bool> mIsStop;
 };
 
-//class CMapDataEmitter: public CActor<boost::mpl::list<Position> >
-//{
-//};
+struct FirstMessage{};
+struct SecondMessage{};
+struct ThirdMessage{};
 
 typedef boost::mpl::vector<FirstMessage, SecondMessage> tMsgTypeList;
 
+class CMapDataEmitter: public CActor<boost::mpl::list<FirstMessage> >
+{
+   typedef CActor<boost::mpl::list<FirstMessage> > tParent;
+public:
+   CMapDataEmitter(CBoard& board) : tParent(board) {}
+
+   virtual void on(boost::shared_ptr<FirstMessage>)
+   {
+   }
+
+   virtual void on(boost::shared_ptr<SecondMessage>)
+   {
+   }
+};
+
 int main(int, char**)
 {
-   std::cout << boost::typeindex::type_id<FirstMessage>().raw_name() << std::endl;
-   std::cout << boost::typeindex::type_id<SecondMessage>().raw_name() << std::endl;
-   std::cout << boost::typeindex::type_id<IHandler<SecondMessage> >().pretty_name() << std::endl;
+   CBoard board;
+   CMapDataEmitter dataEmitter(board);
+   board.publish(boost::shared_ptr<FirstMessage>(new FirstMessage()));
+
+   //std::cout << boost::typeindex::type_id<FirstMessage>().raw_name() << std::endl;
+   //std::cout << boost::typeindex::type_id<SecondMessage>().raw_name() << std::endl;
+   //std::cout << boost::typeindex::type_id<IHandler<SecondMessage> >().pretty_name() << std::endl;
+
+
 
    return 0;
 }
--- visitor.cpp
+++ visitor.cpp
@@ -1,29 +1,97 @@
-#include <iostream>
-#include <vector>
-#include <boost/shared_ptr.hpp>
 #include <boost/foreach.hpp>
+#include <boost/mpl/and.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/greater_equal.hpp>
 #include <boost/mpl/list.hpp>
+#include <boost/mpl/pop_front.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/is_sequence.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread/condition_variable.hpp>
+//#include <boost/type_index.hpp>
+#include <boost/variant.hpp>
+#include <boost/variant/detail/over_sequence.hpp>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
 #include <typeinfo>
+#include <vector>
 
-typedef boost::mpl::list<int, double, std::string> tValueList;
+//typedef boost::mpl::list<int, double, std::string> tValueList;
+typedef boost::mpl::list<int> tValueList;
 
-template <typename T>
+template <typename T, typename = void>
 class VisitorBase
 {
 public:
-   virtual void visit(T& value) { std::cout << typeid(value).name() << " " << value << " visited" << std::endl; }
-};
-
-template <typename FirstType, typename ... Types>
-class VisitorBase<boost::mpl::list<FirstType, Types...> > : public VisitorBase<FirstType>
-                                                          , public VisitorBase<boost::mpl::list<Types...> >
-{
+   virtual void visit(T&) = 0;
 };
 
 template <typename T>
-class VisitorBase<boost::mpl::list<T> > : public VisitorBase<T>
-{
-};
+class VisitorBase
+<
+   T,
+   typename boost::enable_if
+   <
+      boost::mpl::and_
+      <
+         boost::mpl::is_sequence<T>,
+         boost::mpl::greater_equal
+         <
+            boost::mpl::size<T>,
+            boost::mpl::int_<2>
+         >
+      >
+   >::type
+>  : public VisitorBase<typename boost::mpl::front<T>::type>
+   , public VisitorBase<typename boost::mpl::pop_front<T>::type>
+{};
+
+template <typename T>
+class VisitorBase
+<
+   T,
+   typename boost::enable_if
+   <
+      boost::mpl::and_
+      <
+         boost::mpl::is_sequence<T>,
+         boost::mpl::equal_to
+         <
+            boost::mpl::size<T>,
+            boost::mpl::int_<1>
+         >
+      >
+   >::type
+>  : public VisitorBase<typename boost::mpl::front<T>::type>
+{};
+
+//template <typename T>
+//class VisitorBase
+//{
+//public:
+   //virtual void visit(T& value) { std::cout << typeid(value).name() << " " << value << " visited" << std::endl; }
+//};
+
+//template <typename FirstType, typename ... Types>
+//class VisitorBase<boost::mpl::list<FirstType, Types...> > : public VisitorBase<FirstType>
+                                                          //, public VisitorBase<boost::mpl::list<Types...> >
+//{
+//};
+
+//template <typename T>
+//class VisitorBase<boost::mpl::list<T> > : public VisitorBase<T>
+//{
+//};
+
+
+
 
 template <typename ListType>
 struct ValueHolderBase
@@ -61,6 +129,7 @@ struct ValueHolder
 };
 
 typedef boost::mpl::list<int, double, std::string, bool> tTypeList;
+//typedef boost::mpl::list<int> tTypeList;
 
 class PoliVisitor: public VisitorBase<tTypeList>
 {
@@ -70,13 +139,46 @@ class PoliVisitor: public VisitorBase<tTypeList>
    void visit(bool& ) {}
 };
 
+template <typename T>
+struct add_shared_ptr
+{
+   typedef boost::shared_ptr<T> type;
+};
+
+template<class Seq>
+struct shared_ptr_list
+{
+   typedef typename boost::mpl::transform< Seq, add_shared_ptr<boost::mpl::_1> >::type type;
+};
+
+typedef shared_ptr_list<tTypeList>::type tPtrTypeList;
+
+struct TypenamePrinter
+{
+   template <class T>
+   void operator ()(T&)
+   {
+      std::cout << boost::typeindex::type_id<T>().pretty_name() << std::endl;
+   }
+};
+
+typedef ValueHolder<tTypeList> tVariant;
+
+struct Applicator
+{
+   Applicator(PoliVisitor& visitor) : mVisitor(visitor) {}
+   void operator ()(tVariant& v) { v.apply(mVisitor); }
+   PoliVisitor& mVisitor;
+};
+
 int main(int, char**)
 {
    PoliVisitor v;
 
-   typedef ValueHolder<tTypeList> tVariant;
-   std::vector<ValueHolder<tTypeList> > vs;
-   //tVariant v1, v2;
+   boost::mpl::for_each<tPtrTypeList>(TypenamePrinter());
+
+   std::vector<tVariant> vs;
+
    tVariant v1(1);
    tVariant v2(true);
    v1.put(10);
@@ -84,13 +186,14 @@ int main(int, char**)
 
    vs.push_back(v1);
    vs.push_back(v2);
-   vs.emplace_back(42);
-   vs.emplace_back(std::string("wow"));
-   //vs.emplace_back('a');
+   //vs.emplace_back(42);
+   //vs.emplace_back(std::string("wow"));
+   vs.push_back(tVariant(42));
+   vs.push_back(tVariant(std::string("wow")));
 
-   //vs.push_back(tVariant(10));
-   //vs.push_back(tVariant(std::string("hello!"));
-   std::for_each(vs.begin(), vs.end(), [&](ValueHolder<tTypeList>& vh) { vh.apply(v); });
+
+   //std::for_each(vs.begin(), vs.end(), [&](ValueHolder<tTypeList>& vh) { vh.apply(v); });
+   std::for_each(vs.begin(), vs.end(), Applicator(v));
 
    return 0;
 }
