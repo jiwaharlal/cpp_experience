51701287d7835d7703342573fa0b5b763c319bd6
Maxim Bondarenko <mgbondarenko@luxoft.com>
2016-12-26 08:12:31 +0200

    unknown changes

--- boostPractice/src/practiceMain.cpp
+++ boostPractice/src/practiceMain.cpp
@@ -1,7 +1,8 @@
-#define BOOST_THREAD_PROVIDES_FUTURE
+//#define BOOST_THREAD_PROVIDES_FUTURE
 #include <boost/bind.hpp>
 #include <boost/chrono.hpp>
 #include <boost/exception_ptr.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/function.hpp>
 #include <boost/fusion/container/vector.hpp>
 #include <boost/fusion/include/sequence.hpp>
@@ -9,74 +10,72 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/range/algorithm.hpp>
-#include <boost/thread/future.hpp>
+//#include <boost/thread/future.hpp>
 #include <iostream>
 #include <stdexcept>
-#include <tr1/shared_ptr.h>
+//#include <tr1/shared_ptr.h>
 #include <typeinfo>
 
-#include "FutureExpectant.hpp"
+//#include "FutureExpectant.hpp"
 
-struct Gps
-{
-   double lat;
-   double lon;
-};
+//struct Gps
+//{
+   //double lat;
+   //double lon;
+//};
 
-template <typename T>
-struct SharedPtrHolder
-{
-   typedef boost::shared_ptr<T> tPtr;
+//template <typename T>
+//struct SharedPtrHolder
+//{
+   //typedef boost::shared_ptr<T> tPtr;
 
-   SharedPtrHolder(tPtr ptr) : mPtr(ptr) {}
-   tPtr& get() { return mPtr; }
+   //SharedPtrHolder(tPtr ptr) : mPtr(ptr) {}
+   //tPtr& get() { return mPtr; }
 
-   tPtr mPtr;
-};
+   //tPtr mPtr;
+//};
 
-typedef std::tr1::shared_ptr<Gps> tGpsPtr;
-typedef boost::future<tGpsPtr> tGpsFuture;
-typedef boost::shared_future<Gps> tGpsSharedFuture;
+//typedef std::tr1::shared_ptr<Gps> tGpsPtr;
+//typedef boost::future<tGpsPtr> tGpsFuture;
+//typedef boost::shared_future<Gps> tGpsSharedFuture;
 
-boost::future<int> getInt()
-{
-   typedef boost::shared_ptr<boost::promise<int> > tIntPromisePtr;
-   tIntPromisePtr pi(new boost::promise<int>());
-   //struct Functor
-   //{
-      //Functor(tIntPromisePtr& pi) : mPi(pi) {}
-      //void operator ()()
-      //{
-         //boost::this_thread::sleep(boost::posix_time::milliseconds(400));
-         //mPi->set_value(42);
-      //}
+//boost::future<int> getInt()
+//{
+   //typedef boost::shared_ptr<boost::promise<int> > tIntPromisePtr;
+   //tIntPromisePtr pi(new boost::promise<int>());
 
-      //tIntPromisePtr& mPi;
-   //};
-   //Functor f(pi);
-   //boost::thread t(boost::bind(&Functor::operator(), f));
+   //return pi->get_future();
+//}
 
-   return pi->get_future();
-}
+//tGpsFuture getGps()
+//{
+   //tGpsPtr gps(new Gps());
+   //return boost::make_ready_future(gps);
+//}
 
-tGpsFuture getGps()
-{
-   tGpsPtr gps(new Gps());
-   return boost::make_ready_future(gps);
-}
-
-tGpsSharedFuture getSharedGps()
-{
-   return boost::make_shared_future(Gps());
-}
+//tGpsSharedFuture getSharedGps()
+//{
+   //return boost::make_shared_future(Gps());
+//}
 
 int main(int, char**)
 {
-   boost::future<int> fi = getInt();
-   if (fi.is_ready())
-   {
-      std::cout << fi.get() << std::endl;
-   }
+   //boost::future<int> fi = getInt();
+   //if (fi.is_ready())
+   //{
+      //std::cout << fi.get() << std::endl;
+   //}
+
+   using namespace boost::filesystem;
+
+   path p1("/home/mgbondarenko/Documents/map");
+   path p2("/home/mgbondarenko/Documents/map.xml");
+
+   std::string full1 = change_extension(p1, "zxml").string();
+   std::string full2 = change_extension(p2, "zxml").string();
+
+   std::cout << "path 1 " << full1 << std::endl;
+   std::cout << "path 2 " << full2 << std::endl;
 
    return 0;
 }
