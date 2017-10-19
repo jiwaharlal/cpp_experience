3381d2fdd06e1988e3295760a002ea770ee8fb38
Maxim Bondarenko <maxim.bondarenko@apostera.com>
2017-10-19 17:46:37 +0300

    more spirit, employee

--- boost_spirit.cpp
+++ boost_spirit.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
-#include <boost/spirit.hpp>
+#include <boost/spirit/include/qi.hpp>
 #include <boost/range/istream_range.hpp>
+#include <boost/spirit/include/support_istream_iterator.hpp>
 #include <sstream>
+
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+
+template <typename Iterator>
+bool parse_numbers(Iterator first, Iterator last)
+{
+    using qi::double_;
+    using qi::phrase_parse;
+    using ascii::space;
+    using qi::int_;
+    using qi::char_;
+
+    auto date_parser = int_ >> char_('-') >> int_ >> char_('-') >> int_;
+    auto time_parser = int_ >> char_(':') >> int_ >> char_(':') >> int_;
+
+    bool r = phrase_parse(
+        first,
+        last,
+        date_parser >> qi::hold[ +(char_(' '))] >> time_parser,
+        space
+    );
+
+    if (first != last) // fail if we did not get a full match
+    {
+        return false;
+    }
+
+    return r;
+}
 
 int main()
 {
     std::stringstream ss;
-    ss << "2017-10-11 12:23:67";
+    ss << "2017-10-11 12:23:47";
+    //auto stream_range = boost::istream_range<std::stringstream::char_type>(ss);
+    boost::spirit::istream_iterator f(ss), l;
+
+    const std::string& str = ss.str();
+    std::cout << "String: " << str << std::endl;
+    std::cout << "Parse result: " << std::boolalpha << parse_numbers(f, l) << std::endl;
 
     return 0;
 }
