ea482aad102181d2fef6a4e38905cc811d38f750
Maxim Bondarenko <maxim.bondarenko@apostera.com>
2018-08-07 16:00:16 +0300

    calculate glm perpendicular left and right

--- glm_perpendicular.cpp
+++ glm_perpendicular.cpp
@@ -13,15 +13,23 @@ int main()
     const glm::dvec3 start = {0., 0., 0.};
     const glm::dvec3 end = {0., 1., 0.};
 
-    const glm::dvec3 vec = end - start;
+    glm::dvec3 vec = end - start;
 
     std::cout << "vec: " << vec << std::endl;
 
     const glm::dvec3 normal = {0., 0., 1.};
     //const glm::dvec3 perp = glm::perp(vec, normal);
-    const auto perp = glm::cross(vec, normal);
+    auto perp = glm::cross(vec, normal);
 
     std::cout << "perp: " << perp << std::endl;
+
+    vec = glm::dvec3{1., 0., 0.};
+
+    const glm::dvec3 up{0., 1., 0.};
+    const glm::dvec3 down{0., -1., 0.};
+
+    std::cout << "up : " << glm::cross(vec, up) << '\n';
+    std::cout << "down : " << glm::cross(vec, down) << '\n';
 
     return 0;
 }
