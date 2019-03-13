50f8e2352687a99a1a738eaa523841596aa1d18e
Maxim Bondarenko <maxim.bondarenko@apostera.com>
2019-03-13 16:49:02 +0200

    virtual functor

---
+++ 2019/virtual-functor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <functional>
+#include <memory>
+
+class A
+{
+    virtual void foo() { std::cout << "A::foo()\n"; }
+};
+
+class B : public A
+{
+    void foo() override { std::cout << "B::foo()\n"; }
+};
+
+int main()
+{
+    std::vector<std::shared_ptr<A>> as;
+
+    return 0;
+}
