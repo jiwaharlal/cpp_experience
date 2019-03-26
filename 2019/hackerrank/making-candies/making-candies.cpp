9010ea013cff84f0a3b192f86433223d5318dc48
Maxim Bondarenko <maxim.bondarenko@apostera.com>
2019-03-26 16:05:14 +0200

    making candies, in process

---
+++ 2019/hackerrank/making-candies/making-candies.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+std::pair<long, long>
+
+// Complete the minimumPasses function below.
+long minimumPasses(long m, long w, long p, long n)
+{
+    std::vector<std::pair<long, long>> prod;
+    long cache = 0;
+
+    prod.emplace_back(m, w);
+
+    while (m * w < n)
+    {
+        cache += m * w;
+        long add_prod = cache / p;
+        cache -= add_prod * p;
+
+        std::tie(m, w) = calcNewProds(m, w, add_prod);
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string mwpn_temp;
+    getline(cin, mwpn_temp);
+
+    vector<string> mwpn = split_string(mwpn_temp);
+
+    long m = stol(mwpn[0]);
+
+    long w = stol(mwpn[1]);
+
+    long p = stol(mwpn[2]);
+
+    long n = stol(mwpn[3]);
+
+    long result = minimumPasses(m, w, p, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string)
+{
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
