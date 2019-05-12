3ca9b57e3e0f6a529fde5d41fe10999c28f3e9f5
Maxim Bondarenko <maxim.bondarenko@apostera.com>
2019-05-12 14:10:43 +0300

    connected-components done

--- 2019/hackerrank/connected-components/connected-components.cpp
+++ 2019/hackerrank/connected-components/connected-components.cpp
@@ -7,12 +7,87 @@ using namespace std;
 /*
  * Complete the componentsInGraph function below.
  */
+void trackComponent(
+        int component_idx,
+        const AdjList& adj,
+        std::vector<int>& vertex_components,
+        int initial_vertex)
+{
+    std::vector<int> stack;
+    stack.push_back(initial_vertex);
+    vertex_components[initial_vertex] = component_idx;
+
+    while (!stack.empty())
+    {
+        int cur = stack.back();
+        stack.pop_back();
+
+        for (int v : adj[cur])
+        {
+            if (vertex_components[v] == component_idx)
+            {
+                continue;
+            }
+
+            vertex_components[v] = component_idx;
+            stack.push_back(v);
+        }
+    }
+}
+
+std::vector<int> findComponents(const AdjList& adj)
+{
+    std::vector<int> vertex_components(adj.size(), -1);
+
+    int cur_component = 0;
+
+    for (int i = 0; i < vertex_components.size(); ++i)
+    {
+        if (vertex_components[i] == -1)
+        {
+            trackComponent(cur_component, adj, vertex_components, i);
+            ++cur_component;
+        }
+    }
+
+    return vertex_components;
+}
+
 vector<int> componentsInGraph(vector<vector<int>> gb) {
     /*
      * Write your code here.
      */
+    for (auto& vertices : gb)
+    {
+        for (auto& v : vertices)
+        {
+            --v;
+        }
+    }
+
     auto adj = buildGraph(gb);
-    return adj[0];
+    auto vertex_components = findComponents(adj);
+    auto components_count =
+        *std::max_element(vertex_components.begin(), vertex_components.end()) + 1;
+
+    std::vector<int> components_weight(components_count, 0);
+    for (auto c : vertex_components)
+    {
+        ++components_weight[c];
+    }
+
+    int max_weight = *std::max_element(components_weight.begin(), components_weight.end());
+    int min_weight = max_weight;
+
+    for (int w : components_weight)
+    {
+        if (w > 1 && w < min_weight)
+        {
+            min_weight = w;
+        }
+    }
+
+    return {min_weight, max_weight};
 }
 
 int main()
